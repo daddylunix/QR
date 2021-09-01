c988030ef162b35f93a8cf9f57cd1ad544b04ba9
daddylunix <ramezjoseph8@gmail.com>
2021-09-01 05:47:36 +0200

    Established DB Connection

--- server/server.js
+++ server/server.js
@@ -2,19 +2,32 @@ const express = require('express');
 const app = express();
 const mongoose = require("mongoose");
 const User = require("./models/User");
+require('dotenv').config()
+
+const password = process.env.USER_PASSWORD
+console.log(password);
 
 const db = async () => {
 try {
-    mongoose.connect('mmongodb+srv://lunix:<password>@qr-project.j6yuz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true});
+    mongoose.connect(`mongodb+srv://admin:${password}@qr-project.j6yuz.mongodb.net/QR?retryWrites=true&w=majority`, {useNewUrlParser: true}, () => {
+        console.log('connected to mongodb.')
+    });
 } catch (error) {
     console.error(err);
 }
 }
+db();
 
 app.get('/', async (req, res) => {
-    const user = await new User();
-    const saveduser = await user.save();
-    res.json(user);
+    const user = await new User({
+        username:'hellothere'
+    });
+    try {
+        const saveduser = await user.save();
+        res.json(user);
+    } catch (error) {
+        res.json(error)
+    }
 })
 
 app.listen(5000, () => {
