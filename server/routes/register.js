e1ae99553d3209da01cd4de5ac60d24d28431572
daddylunix <ramezjoseph8@gmail.com>
2021-09-01 19:07:21 +0200

    🔥 Register auth

--- server/routes/register.js
+++ server/routes/register.js
@@ -2,12 +2,24 @@ const router = require('express').Router();
 const User = require('../models/User');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
 
 router.post('/register', async (req, res) => {
+    const bcryptSalt = await bcrypt.genSalt(13);
+    const hashedPassword = await bcrypt.hash(req.body.password, bcryptSalt);
+    const usernameExists = await User.findOne({username: req.body.username});
+    const emailExists = await User.findOne({email:req.body.email});
+    
+    if (!req.body.username || !req.body.email || !req.body.password ) {
+        return res.json('Credentials missing')
+    }
+    if (usernameExists || emailExists) {
+        return res.json("User already exists")
+    };
     const user = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
     });
     try {
         const savedUser = await user.save();
