a1011d95bcdee0d126e4279ec8cd1c86c2320181
daddylunix <ramezjoseph8@gmail.com>
2021-09-07 05:22:36 +0200

    🚀 [@id].js

---
+++ client/pages/[@id].js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+
+export default function id () {
+    const router = useRouter()
+    const id = router.query["@id"]
+
+  return <p>Post: {id}</p>
+}
