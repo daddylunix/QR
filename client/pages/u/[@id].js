1f9cc4c08a6b166c44418249182807f3f757eb93
daddylunix <ramezjoseph8@gmail.com>
2021-09-19 19:25:27 +0200

    🚀  /u/@username

---
+++ client/pages/u/[@id].js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react'
+import { useRouter  } from 'next/router'
+import axios from 'axios';
+
+export default function id () {
+    const [ response, setResponse ] = useState("");
+    const router = useRouter();
+    // const id = router.query["@id"];
+    const id = router.query["@id"];
+    const testFunction = async (id) => {
+      const config = {
+        header: {
+            "Content-Type":"application/json",
+            Credentials: true
+        },
+        withCredentials: true
+    }
+      if (id === undefined) return; 
+      const response = await axios.get(`http://localhost:5000/${id}`, config);
+      setResponse(response.data);
+      console.log(response.data);
+      console.log(id);
+    }
+    useEffect(() => {
+      testFunction(id)
+    }, [id])
+    return (
+      <div>
+    <p>params: {id}</p>
+    <h2>{JSON.stringify(response)}</h2>
+    </div>
+  ) 
+}
