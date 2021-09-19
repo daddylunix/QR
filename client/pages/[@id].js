346b1d16b8647ecf5b2ee633452f3c2b637f562f
daddylunix <ramezjoseph8@gmail.com>
2021-09-19 13:57:30 +0200

    🐛  [id].js

--- client/pages/[@id].js
+++ client/pages/[@id].js
@@ -1,9 +1,14 @@
-import React from 'react'
-import { useRouter } from 'next/router'
+import React, { useState, useEffect } from 'react'
+import { useRouter  } from 'next/router'
+import axios from 'axios';
 
 export default function id () {
-    const router = useRouter()
-    const id = router.query["@id"]
-
-  return <p>Post: {id}</p>
+    const [ response, setResponse ] = useState("");
+    const router = useRouter();
+    const id = router.query["@id"];
+    return (
+      <div>
+    <p>params: {id}</p>
+    </div>
+  ) 
 }
